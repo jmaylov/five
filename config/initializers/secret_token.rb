a99e7cf75267f1543e2953c21356d5788c975673
jmaylov <jmaylov@gmail.com>
2012-04-07 01:57:14 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Five::Application.config.secret_token = 'd73c4e006d8df76ed387496d0c8cb5ffee6d84aa37731fc56eadd25be8664a222a1e167959608f13e39a0e1faab9fd419be9d8eada38461f77de302c6ff2c11e'
